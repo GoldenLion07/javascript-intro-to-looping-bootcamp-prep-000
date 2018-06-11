17dd03679d67993afa700e3bb9d3d56148f78733
gmat191@gmail.com <gmat191@gmail.com>
2018-06-11 18:05:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,4 +8,14 @@ function forLoop (array) {
 
 function whileLoop(number){
   while (n > 0)
+  {console.log(n--)}
+  return 'done'
+}
+
+function maybeTrue(){
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  
 }
