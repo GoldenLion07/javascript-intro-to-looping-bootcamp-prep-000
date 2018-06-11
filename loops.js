8eeaa282052cbfc30a174b6c8d205293dff68228
gmat191@gmail.com <gmat191@gmail.com>
2018-06-11 18:03:15 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop (array) {
+  for (let i = 0; i < 25; i++)
+  if (i === 1) {
+    return "I am 1 strange loop."
+  }
+  
+}
+function whileLoop(number){
+  let countdown = 100;
+  while (countdown > 0){
+    console.log('done')
+  }
+}
+ 
+ function doWhileLoop(array){
+   
+ }
